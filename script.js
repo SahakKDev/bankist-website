87311bb80f8970116db575949fb7f8173499f8de
Sahak <sahak.keshishyan.dev@gmail.com>
2024-01-04 08:29:29 +0400

    make navigation links click smooth scroll to sections, add cookie message

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
+// Modal window
 const openModal = function (event) {
   event.preventDefault();
 
@@ -31,4 +31,44 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+btnScrollTo.addEventListener('click', () => {
+  section1.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
+
+// Page navigation
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const id = e.target.getAttribute('href');
+  if (e.target.classList.contains('nav__link') && !id.endsWith('#')) {
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+});
+
 //////////////////////////////////////
+//////////////////////////////////////
+//////////////////////////////////////
+
+const header = document.querySelector('.header');
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = `
+  We use cookies for improved functionality and analytics.
+  <button class='btn btn--close-cookie'>Got it!</button>
+`;
+
+header.prepend(message);
+
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  message.remove();
+});
+
+message.style.backgroundColor = '#37383d';
+
+message.style.height = parseFloat(getComputedStyle(message).height) + 30 + 'px';
