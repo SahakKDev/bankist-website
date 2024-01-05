b469983f08706d296ae539fdcba95f9a6d60f729
Sahak <sahak.keshishyan.dev@gmail.com>
2024-01-05 08:24:12 +0400

    create tabbed component functionality, menu fade animation, sticky navigation

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
+const header = document.querySelector('.header');
+const message = document.createElement('div');
 const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 // Modal window
 const openModal = function (event) {
@@ -38,7 +44,6 @@ btnScrollTo.addEventListener('click', () => {
 });
 
 // Page navigation
-
 document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
 
@@ -50,13 +55,67 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+// Tabbed component
+tabsContainer.addEventListener('click', function (e) {
+  const tab = e.target.closest('.operations__tab');
+
+  if (!tab) return;
+
+  // Remove active classes
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach(tc => {
+    tc.classList.remove('operations__content--active');
+  });
+
+  tab.classList.add('operations__tab--active');
+  const id = tab.dataset.tab;
+  document
+    .querySelector(`.operations__content--${id}`)
+    .classList.add('operations__content--active');
+});
+
+// Menu fade animation
+function onNavMouseEvent(e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+}
+
+nav.addEventListener('mouseover', onNavMouseEvent.bind(0.5));
+nav.addEventListener('mouseout', onNavMouseEvent.bind(1));
+
+// Sticky navigation
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+};
+
+const navHeight = nav.getBoundingClientRect().height;
+const obsOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, obsOptions);
+headerObserver.observe(header);
+
 //////////////////////////////////////
 //////////////////////////////////////
 //////////////////////////////////////
 
-const header = document.querySelector('.header');
-
-const message = document.createElement('div');
 message.classList.add('cookie-message');
 message.innerHTML = `
   We use cookies for improved functionality and analytics.
